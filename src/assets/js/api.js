4afd4dacb4e10d1671926161ad6cf0b362bc676a
wuyue <wuyue@idstaff.com>
2020-12-01 21:55:54 +0800

    项目初始化

---
+++ src/assets/js/api.js
@@ -0,0 +1,7 @@
+export default {
+  //商拼详情
+  GetMaterialGoodsById: '/api/APP_Detail/APP_GetGoodsinfo',
+  clickTobuy: '/api/Detail/clickTobuy/', //点击购物
+  GetUserIsKeepOnRecord: '/api/PDD/GetUserIsKeepOnRecord',//查看用户是否拼多多升级授权
+  UserKeepOnRecord: '/api/PDD/UserKeepOnRecord', //获取拼多多授权链接
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  data() {
+    return
+    {
+
+    }
+  },
+  mounted() {
+    window.location.href = `yldauction://?url=${window.encodeURIComponent('http://192.168.199.125:8080/#/productDetail?goodsType=2&goodsId=623389024792')}`
+  }
+}
+</script>
